ef8a6f6f997b492faa3070920fbcd804a603fdc8
Jessica Söderback <jessicavilja@gmail.com>
2022-02-05 19:55:23 +0100

    påbörjat styling av sidan mha bootstrap och css

--- main.js
+++ main.js
@@ -11,25 +11,31 @@ function printPosts() {
     const item = post.posts[i];
 
     const divElement = document.createElement("div");
-    divElement.className = "blogpost-items";
+    divElement.className = "blogpost-items mb-3";
 
     const liElement = document.createElement("li");
-    liElement.className = "blogpost-item";
+    liElement.className = "blogpost-item text-center";
 
     const title = document.createElement("h2");
     const image = document.createElement("img");
     image.className = "blogpost-image";
     const content = document.createElement("p");
+
     const dateEl = document.createElement("span");
+    //dateEl.className = "d-flex justify-content-start";
+
     const removeBlogpost = document.createElement("button");
+    removeBlogpost.className = "btn btn-outline-dark";
 
     title.textContent = item.title;
     image.src = item.image;
     content.textContent = item.content;
     dateEl.textContent = item.date;
-    removeBlogpost.innerHTML = "X";
+    removeBlogpost.innerHTML = "x";
 
     liElement.append(image);
+    liElement.append(title);
+
     liElement.append(content);
     liElement.append(dateEl);
     liElement.append(removeBlogpost);
